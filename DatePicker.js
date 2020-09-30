99d8c9173b2495b44fcd0a5c37d282c624430762
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2020-10-01 02:02:32 +0800

    task 1 finished

--- DatePicker.js
+++ DatePicker.js
@@ -8,31 +8,7 @@ class DatePicker {
       day: null,
       year: null,
     };
-  }
-
-  render(date) {
-    const element = document.getElementById(this.id);
-    this.date = {
-      month: date.getMonth() + 1,
-      day: date.getDate(),
-      year: date.getFullYear(),
-    };
-
-    this.callback(this.id, this.date);
-    const table = this.generateMonthTable();
-    element.innerHTML = table;
-  }
-
-  getFirstLastWeekday(date) {
-    // console.log('date:', date);
-    return {
-      first: new Date(`${date.year}-${date.month}-01`).getDay(),
-      numOfDays: new Date(date.year, date.month, 0).getDate(),
-    };
-  }
-
-  generateMonthTable() {
-    const months = [
+    this.months = [
       'January',
       'February',
       'March',
@@ -46,18 +22,71 @@ class DatePicker {
       'November',
       'December',
     ];
+    this.week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+  }
 
-    let table = `<h2>${months[this.date.month - 1]}, ${this.date.year}</h2>`;
-    table += '<table><tr>';
-    table +=
-      '<td>Su</td><td>Mo</td><td>Tu</td><td>We</td><td>Th</td><td>Fr</td><td>Sa</td></tr>';
+  render(date) {
+    const element = document.getElementById(this.id);
+    this.date = {
+      month: date.getMonth() + 1,
+      day: date.getDate(),
+      year: date.getFullYear(),
+    };
 
+    this.callback(this.id, this.date);
+    const table = this.generateMonthTable();
+
+    // create buttons
+    const buttonPrev = document.createElement('button');
+    buttonPrev.appendChild(document.createTextNode('Prev'));
+
+    buttonPrev.setAttribute('class', 'btn-prev');
+    buttonPrev.addEventListener('click', () => {
+      this.onPrevClick();
+      this.callback(this.id, this.date);
+    });
+
+    const monthYear = document.createElement('h2');
+    monthYear.appendChild(
+      document.createTextNode(
+        `${this.months[this.date.month - 1]}, ${this.date.year}`
+      )
+    );
+
+    const buttonNext = document.createElement('button');
+    buttonNext.appendChild(document.createTextNode('Next'));
+
+    buttonNext.setAttribute('class', 'btn-next');
+    buttonNext.addEventListener('click', () => {
+      this.onNextClick();
+      this.callback(this.id, this.date);
+    });
+
+    const dateElement = document.createElement('div');
+    dateElement.setAttribute('class', 'date');
+    dateElement.appendChild(table);
+
+    element.appendChild(buttonPrev);
+    element.appendChild(monthYear);
+    element.appendChild(buttonNext);
+    element.appendChild(dateElement);
+  }
+
+  getFirstLastWeekday(date) {
+    // console.log('date:', date);
+    return {
+      first: new Date(`${date.year}-${date.month}-01`).getDay(),
+      numOfDays: new Date(date.year, date.month, 0).getDate(),
+    };
+  }
+
+  generateMonthTable() {
     const { first: firstDay, numOfDays } = this.getFirstLastWeekday(this.date);
 
     // get sibling months
     const prevMonth = this.getFirstLastWeekday({
       ...this.date,
-      month: this.date.month - 1 === 0 ? 12 : this.date.month - 1,
+      month: this.date.month === 1 ? 12 : this.date.month - 1,
     });
 
     const limit =
@@ -65,25 +94,96 @@ class DatePicker {
         ? 7 * (parseInt((firstDay + numOfDays) / 7) + 1)
         : 7 * ((firstDay + numOfDays) / 7);
 
-    console.log(limit);
-    for (let i = 0, day = 1; i < limit; i++) {
-      if (i === 0) table += '<tr>';
-      if (i !== 0 && i % 7 === 0) table += '</tr><tr>';
+    const table = document.createElement('table');
+
+    const week = document.createElement('tr');
+
+    this.week.map((day) => {
+      const col = document.createElement('td');
+      col.appendChild(document.createTextNode(day));
+      week.appendChild(col);
+    });
+
+    table.appendChild(week);
+
+    for (let i = 0, row, col, day = 1; i < limit; i++) {
+      if (i % 7 === 0) {
+        row && table.appendChild(row);
+        row = document.createElement('tr');
+      }
+
       if (i < firstDay) {
-        table += `<td class="dim">${
-          prevMonth.numOfDays - (firstDay - 1) + i
-        }</td>`;
+        const day = prevMonth.numOfDays - (firstDay - 1) + i;
+        col = document.createElement('td');
+        col.setAttribute('class', 'dim');
+        col.appendChild(document.createTextNode(day));
+        row.appendChild(col);
       }
       if (i >= firstDay && i < firstDay + numOfDays) {
-        table += `<td>${day}</td>`;
+        col = document.createElement('td');
+        col.appendChild(document.createTextNode(day));
+        col.addEventListener('click', () => {
+          this.callback(this.id, {
+            ...this.date,
+            day: day - 1,
+          });
+        });
+        row.appendChild(col);
         day++;
       }
 
       if (i >= firstDay + numOfDays) {
-        table += `<td class="dim">${i - (numOfDays + firstDay) + 1}</td>`;
+        const day = i - (numOfDays + firstDay) + 1;
+        col = document.createElement('td');
+        col.setAttribute('class', 'dim');
+
+        col.appendChild(document.createTextNode(day));
+        row.appendChild(col);
+      }
+
+      if (i === limit - 1) {
+        table.appendChild(row);
       }
     }
-    table += '</table>';
+
     return table;
+  }
+
+  onPrevClick() {
+    this.date = {
+      ...this.date,
+      month: this.date.month === 1 ? 12 : this.date.month - 1,
+      year: this.date.month === 1 ? this.date.year - 1 : this.date.year,
+    };
+
+    const dateElement = document.getElementById(this.id).lastChild;
+
+    const table = this.generateMonthTable();
+    dateElement.innerHTML = '';
+    dateElement.appendChild(table);
+    this.updateDate();
+
+    // this.render();
+  }
+
+  onNextClick() {
+    this.date = {
+      ...this.date,
+      month: this.date.month === 12 ? 1 : this.date.month + 1,
+      year: this.date.month === 12 ? this.date.year + 1 : this.date.year,
+    };
+
+    const dateElement = document.getElementById(this.id).lastChild;
+    const table = this.generateMonthTable();
+    dateElement.innerHTML = '';
+    dateElement.appendChild(table);
+    this.updateDate();
+    // this.render();
+  }
+
+  updateDate() {
+    document.getElementById(this.id).children[1].innerHTML = `${
+      this.months[this.date.month - 1]
+    }, ${this.date.year}`;
   }
 }
